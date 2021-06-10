c1ea2453926a38f66e8aea990c72423174111014
Oleksii Denyshchyk <63901283+dnshk@users.noreply.github.com>
2021-06-09 22:34:00 -0400

    added search bar for test

--- index.php
+++ index.php
@@ -6,5 +6,10 @@
   <body>
     <h1>Home Page</h2>
     <a href="./products.php">Products</a>
+    <h2> Search for products </h2>
+    <form action="search.php" action="POST">
+    <input type="text" name ="search"><br>
+    <input type="submit" value = "Search">
+    </form>
   </body>
 </html>
